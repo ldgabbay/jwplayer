325ce327677e7fe612549882ac2d21ca9ed9f496
jeroen <jeroen@6bf506d4-4edb-4b42-8bf1-84b36a5c7d31>
2009-08-04 09:55:19 +0000

    added build.bat files to the plugin SDK, plus references to the compilation tutorial
    git-svn-id: http://developer.longtailvideo.com/svn/trunk@259 6bf506d4-4edb-4b42-8bf1-84b36a5c7d31

--- sdk/plugins/docking/build.sh
+++ sdk/plugins/docking/build.sh
@@ -1,5 +1,6 @@
-# This is a simple script that compiles the plugin using MXMLC (free & cross-platform).
-# To use, make sure you have downloaded and installed the Flex SDK in the following directory:
+# This is a simple script that compiles the plugin using the free Flex SDK on Linux/Mac.
+# Learn more at http://developer.longtailvideo.com/trac/wiki/PluginsCompiling
+
 FLEXPATH=/Developer/SDKs/flex_sdk_3
 
 
