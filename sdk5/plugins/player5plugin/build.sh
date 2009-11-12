9185171da1c13d2e26c38da031742dcfec91b82b
pablo <pablo@6bf506d4-4edb-4b42-8bf1-84b36a5c7d31>
2009-11-12 03:15:00 +0000

    Updating player sdk swc
    git-svn-id: http://developer.longtailvideo.com/svn/trunk@617 6bf506d4-4edb-4b42-8bf1-84b36a5c7d31

--- sdk5/plugins/player5plugin/build.sh
+++ sdk5/plugins/player5plugin/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This is a simple script that compiles the plugin using the free Flex SDK on Linux/Mac.
 # Learn more at http://developer.longtailvideo.com/trac/wiki/PluginsCompiling
 
@@ -5,4 +6,4 @@ FLEXPATH=/Developer/SDKs/flex_sdk_3
 
 
 echo "Compiling positioning plugin..."
-$FLEXPATH/bin/mxmlc ./Player5Plugin.as -sp ./ -o ./player5plugin.swf -library-path ./lib -load-externs sdk-classes.xml -use-network=false
+$FLEXPATH/bin/mxmlc ./Player5Plugin.as -sp ./ -o ./player5plugin.swf -library-path ./sdk/lib -load-externs sdk-classes.xml -use-network=false
