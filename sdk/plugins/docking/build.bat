82df320867a038b1c3fd28147fc5a5c3a78be8c2
zach <zach@6bf506d4-4edb-4b42-8bf1-84b36a5c7d31>
2010-01-13 22:19:25 +0000

    - moving JW4 SDK
    git-svn-id: http://developer.longtailvideo.com/svn/trunk@785 6bf506d4-4edb-4b42-8bf1-84b36a5c7d31

--- sdk/plugins/docking/build.bat
+++
@@ -1,8 +0,0 @@
-:: This is a simple script that compiles the plugin using the free Flex SDK on Windows.
-:: Learn more at http://developer.longtailvideo.com/trac/wiki/PluginsCompiling
-
-SET FLEXPATH="\Program Files\flex_sdk_3"
-
-echo "Compiling docking plugin..."
-
-%FLEXPATH%\bin\mxmlc .\Docking.as -sp .\ -o .\docking.swf -use-network=false
