b562b251e066d8eda49b5e679dd6b4bdf58005e2
pablo <pablo@6bf506d4-4edb-4b42-8bf1-84b36a5c7d31>
2011-05-16 15:45:49 +0000

    Fixes an issue parsing <source> tags in IE8 and below.
    git-svn-id: http://developer.longtailvideo.com/svn/trunk@1792 6bf506d4-4edb-4b42-8bf1-84b36a5c7d31

--- fl5/js/bin-debug/jwplayer.js
+++ fl5/js/bin-debug/jwplayer.js
@@ -18,8 +18,12 @@ var jwplayer = function(container) {
 
 var $jw = jwplayer;
 
-jwplayer.version = '5.6.1791';
-/**
+jwplayer.version = '5.6.1792';
+
+// "Shiv" method for older IE browsers; required for parsing media tags
+jwplayer.vid = document.createElement("video");
+jwplayer.audio = document.createElement("audio");
+jwplayer.source = document.createElement("source");/**
  * Utility methods for the JW Player.
  *
  * @author zach, pablo
--- fl5/js/src/jwplayer.js
+++ fl5/js/src/jwplayer.js
@@ -10,4 +10,9 @@ var jwplayer = function(container) {
 
 var $jw = jwplayer;
 
-jwplayer.version = '5.6.1791';
+jwplayer.version = '5.6.1792';
+
+// "Shiv" method for older IE browsers; required for parsing media tags
+jwplayer.vid = document.createElement("video");
+jwplayer.audio = document.createElement("audio");
+jwplayer.source = document.createElement("source");
