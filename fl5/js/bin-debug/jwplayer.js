6d56570478bdaa1d779dbf1238d9d12330bb7789
pablo <pablo@6bf506d4-4edb-4b42-8bf1-84b36a5c7d31>
2012-06-20 16:52:04 +0000

    Fixing inadvertent check in of player binary
    git-svn-id: http://developer.longtailvideo.com/svn/trunk@2273 6bf506d4-4edb-4b42-8bf1-84b36a5c7d31

--- fl5/js/bin-debug/jwplayer.js
+++ fl5/js/bin-debug/jwplayer.js
@@ -18,7 +18,7 @@ var jwplayer = function(container) {
 
 var $jw = jwplayer;
 
-jwplayer.version = '5.9.2268';
+jwplayer.version = '5.9.2273';
 
 // "Shiv" method for older IE browsers; required for parsing media tags
 jwplayer.vid = document.createElement("video");
